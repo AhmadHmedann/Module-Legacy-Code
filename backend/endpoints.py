a5e74d56ca2df5822d9c27ffb71bae37ee9666c7
Ahmad Hmedan <ahmadHmedann@gmail.com>
2026-09-19 00:35:04 +0100

    fix: enforce 280 character bloom limit on the server

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -156,9 +156,20 @@ def send_bloom():
     if type_check_error is not None:
         return type_check_error
 
+    content = request.json["content"]
+    if len(content)>280:
+        return make_response(
+            (
+                {
+                "success": False,
+                "message": "Bloom must be 280 characters or fewer"
+                }
+            ),
+            400,
+        )
     user = get_current_user()
 
-    blooms.add_bloom(sender=user, content=request.json["content"])
+    blooms.add_bloom(sender=user, content=content)
 
     return jsonify(
         {
